d7cf8c5499bfa0af6a141a2ce2457e84901b7067
Skarenno <bonaventura.testa@gmail.com>
2025-10-11 19:28:01 +0200

    User Info Service

--- src/api/user-service.ts
+++ src/api/user-service.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 
-export const user_service_api = axios.create({
-  baseURL: "http://localhost:8001", // Your FastAPI base URL
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8001";
+
+const api = axios.create({
+  baseURL: `${API_BASE_URL}/user`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-
-export const registerUser = async (data: { username: string; email: string; password: string }) => {
-    return user_service_api.post("/user/register", data);
+export const registerUser = async (data: { email: string; password: string }) => {
+    return api.post("/register", data);
   };
   
   export const loginUser = async (data: { username: string; password: string }) => {
-    return user_service_api.post("/user/login", data);
+    return api.post("/login", data);
   };
